cdd8088f40d331dbca1d89c59aa58296ebb3028d
stephen <94320530+stephen-kern@users.noreply.github.com>
2023-05-22 12:36:51 -0500

    cleaned up the detail page component and implemented global state

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,25 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { useStoreContext } from "../utils/GlobalState";
+import { UPDATE_PRODUCTS } from "../utils/actions";
 
-import { QUERY_PRODUCTS } from '../utils/queries';
-import spinner from '../assets/spinner.gif';
+import { QUERY_PRODUCTS } from "../utils/queries";
+import spinner from "../assets/spinner.gif";
 
 function Detail() {
+  const [state, dispatch] = useStoreContext();
   const { id } = useParams();
-
   const [currentProduct, setCurrentProduct] = useState({});
-
   const { loading, data } = useQuery(QUERY_PRODUCTS);
-
-  const products = data?.products || [];
+  const { products } = state;
 
   useEffect(() => {
     if (products.length) {
       setCurrentProduct(products.find((product) => product._id === id));
+    } else if (data) {
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: data.products,
+      });
     }
-  }, [products, id]);
+  }, [products, data, dispatch, id]);
 
+  // The useEffect() hook is used to check if there is data in the global state's products array. If there is, the current product is displayed. If there is no data, the product data is fetched from the useQuery() hook and saved to the global state object. The current product is then displayed.
+  // The current product is saved locally because it is only used in this specific component. Saving it to the global state object would not benefit us in any way.
+  
   return (
     <>
       {currentProduct ? (
@@ -31,7 +39,7 @@ function Detail() {
           <p>{currentProduct.description}</p>
 
           <p>
-            <strong>Price:</strong>${currentProduct.price}{' '}
+            <strong>Price:</strong>${currentProduct.price}{" "}
             <button>Add to Cart</button>
             <button>Remove from Cart</button>
           </p>
